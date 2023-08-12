3cb06de88da25e328e09365212426acc0ca333b3
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-08-12 12:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+document.addEventListener("DOMContentLoaded", function() {
+    const urlForm = document.getElementById("urlForm");
+    const urlText = document.getElementById("url");
+    
+    urlForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const name = encodeURIComponent(document.getElementById("name").value);
+        const year = encodeURIComponent(document.getElementById("year").value);
+        
+        const queryString = `?name=${name}&year=${year}`;
+        const fullURL = `https://localhost:8080/${queryString}`;
+        
+        urlText.textContent = fullURL;
+    });
+});
